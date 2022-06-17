8cad857a68044d4fee15fe7cf75cfe770045557a
Person <localboxcrox@gmail.com>
2022-06-17 23:31:25 +0530

    task left: build the alt listes

--- unban-words.c
+++ unban-words.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 void print_help() {
-    printf("\n🚀  Usage: unbanwd <banned word>\n    For help: unbanwd help\n\n✏️  e.g. 1: unbanwd jingle\n✏️  e.g. 2: unbanwd bells\n\n👋  Bye!\n");
+    printf("\n🚀  Usage: unbanwd <banned word>\n    For help: unbanwd help\n\n✏️  e.g. 1: unbanwd ligma\n✏️  e.g. 2: unbanwd balls\n\n👋  Bye!\n");
 }
 
 //i write my own string length function 💪
-int lenstr(char *str) {
+unsigned int lenstr(char *str) {
     int size = 0;
     while (str[size] != '\0') size++;
     return(size);
@@ -21,10 +21,11 @@ unsigned short cmpstr(char *str1, char *str2) {
     return(0);
 }
 
+
 int main(int argc, char **argv) {
     //first deal with argument errors
     if (argc != 2) {
-        printf("⚠️   Too few/many arguments.\n");
+        printf("\n⚠️   Too few/many arguments.\n");
         print_help();
         return(1);
     } else if (cmpstr(argv[1], "help") == 0) {
@@ -32,12 +33,29 @@ int main(int argc, char **argv) {
         return(0);
     }
 
+    char **alt_lower = {};
+    char **alt_upper = {};
+    unsigned short index;
+
     //do the stuff
     for (int z = 0; z < lenstr(argv[1]); z++) {
-        //
+        //make sure letter is ascii [no buffer overflow protection for utf-8]
+        if (argv[1][z] > 255) {
+            printf();
+            return(1);
+        }
+        
+        //do the stuff stuff
+        if ((argv[1][z] >= 65)&&(argv[1][z] <= 90)) { //lower case
+            printf("%s", alt_upper[argv[1][z]%65]);
+        } else if ((argv[1][z] >= 97)&&(argv[1][z] <= 122)) { //upper case
+            printf("%s", alt_upper[argv[1][z]%97]);
+        } else {
+            printf("%s", argv[1][z]);
+        }
     }
 
     //test
-    printf("%s\n%c\n", argv[1], 0x0455);
+    printf("%d\n%c\n", lenstr(argv[1]), 0x0455);
     return(0);
 }
